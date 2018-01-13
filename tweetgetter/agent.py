603cfa5aabaf1034df8a127317875c7726a7715a
Shota Shimazu <hornet.live.mf@gmail.com>
2018-01-14 01:37:42 +0900

    add avoidance of Twitter API limitation

--- tweetgetter/agent.py
+++ tweetgetter/agent.py
@@ -6,6 +6,11 @@
 #   Copyright (c) 2018 Shota Shimazu. All rights reserved.
 #
 
+from time import sleep
+
 def background_agent(func):
     while True:
+        
+        # Avoid Twitter API request regulation
+        sleep(1)
         func()
